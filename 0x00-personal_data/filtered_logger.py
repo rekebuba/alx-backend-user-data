fe56d9de4ddcf35a2660e780e7ce6768cfc4b517
rekebuba <abukiabdullahi1@gmail.com>
2024-08-29 23:32:12 +0300

    0x00-personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -85,6 +85,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """Constructor"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
