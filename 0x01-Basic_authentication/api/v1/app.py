75b20cac3f322fa1822aaf2ea7a274a1561605f2
rekebuba <abukiabdullahi1@gmail.com>
2024-09-04 00:54:53 +0300

    0x01-Basic_authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Route module for the API.
 """
-import os
 from os import getenv
-from flask import Flask, jsonify, abort, request, Response
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 
 from api.v1.views import app_views
@@ -23,21 +22,21 @@ if auth_type == 'basic_auth':
 
 
 @app.errorhandler(404)
-def not_found(error) -> tuple[Response, int]:
+def not_found(error) -> str:
     """Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> tuple[Response, int]:
+def unauthorized(error) -> str:
     """Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> tuple[Response, int]:
+def forbidden(error) -> str:
     """Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
@@ -64,5 +63,5 @@ def authenticate_user():
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = int(getenv("API_PORT", "5000"))
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
