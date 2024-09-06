542a9373e294df66bc9382f8c4556f1a3a5341d5
rekebuba <abukiabdullahi1@gmail.com>
2024-09-06 23:42:08 +0300

    0x01-Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,14 +15,14 @@ class Auth:
         if path is not None and excluded_paths is not None:
             for exclusion_path in map(lambda x: x.strip(), excluded_paths):
                 pattern = ''
-                # if exclusion_path[-1] == '*':
-                #     pattern = '{}.*'.format(exclusion_path[0:-1])
-                # if exclusion_path[-1] == '/':
-                #     pattern = '{}/*'.format(exclusion_path[0:-1])
-                # else:
-                #     pattern = '{}/*'.format(exclusion_path)
-                # if re.match(pattern, path):
-                return False
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                if exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
